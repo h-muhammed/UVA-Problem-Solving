04a87e8a8c6bca5f66751c09fb8e2bf3ca8bde54
unknown <mshossaincse@gmail.com>
2023-05-24 17:01:18 +0600

    making few updates on existing sources.

--- code-forces/cf_practice1_A.c
+++ code-forces/cf_practice1_A.c
@@ -6,15 +6,11 @@ int main()
     while(scanf("%d %d %d %d", &x1, &y1, &x2, &y2)==4)
     {
         if(x1==x2)
-
-
         printf("%d %d %d %d\n", x1+y2-y1, y1, x2+y2-y1, y2);
-
         else if(y1==y2)
         printf("%d %d %d %d\n", y1+x2-x1, x1, y2+x2-x1, x2);
         else if()
         printf("%d %d %d %d\n", x1, y2, x2, y1);
-
     }
     return 0;
 }
